2edf71c42860d0a9fc2c3a3ddf67b84413dbc08d
Renato Mor <renato.moreira@uni9.edu.br>
2026-05-10 14:29:15 -0300

    versao_1.1.0 - Inclusao da aula 010

---
+++ src/exercicos/conteudo/aula010.py
@@ -0,0 +1,9 @@
+# Execício realizado em: 10/05/2026
+
+concatenacao = "Renato" + " " + "Moreira"
+print(concatenacao)
+
+a_dez_vezes = "A" * 10
+tres_vezes_luiz = 3 * "Renato"
+print(a_dez_vezes)
+print(tres_vezes_luiz)
