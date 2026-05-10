b0ae5f37e3645fdfee38cba3a7a8833b54cecc51
Renato Mor <renato.moreira@uni9.edu.br>
2026-05-10 14:39:01 -0300

    versao_1.1.2 - Inclusao da aula 012

---
+++ src/exercicos/conteudo/aula012.py
@@ -0,0 +1,10 @@
+# Aula realizada em: 10/05/2026
+
+nome = "Renato Moreira"
+altura = 1.68
+peso = 86
+imc = peso / altura**2
+
+print(f"{nome} tem {altura} de altura")
+print(f"Pesa {peso} kg e seu IMC e: {imc:.2f}")
+print(f"{imc:.2f}")
