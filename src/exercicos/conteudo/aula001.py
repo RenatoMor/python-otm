021e01244e06a15f5d56aadda116c40ad9a938db
Renato Mor <renato.moreira@uni9.edu.br>
2026-05-10 13:49:33 -0300

    version_1.0.1 - Inclusao da aula 001

---
+++ src/exercicos/conteudo/aula001.py
@@ -0,0 +1,16 @@
+# Execícios realizado em: 10/05/2026
+"""
+DocString
+E escrever o que eu
+quiser
+
+
+asdfasdfd
+"""
+
+""" Usar para escrever suas notas """
+
+# Permite escrever um comentário
+print(123)  # Na frente
+# Abaixo
+print(456)
